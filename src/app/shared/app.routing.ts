535115a3ec5ad7fd693ce5e6f36decb42f4f555c
Hitesh Khatri <khatrih@reedexpo.co.uk>
2017-09-14 15:07:24 +0100

    blog completed starting shopping cart

--- src/app/shared/app.routing.ts
+++ src/app/shared/app.routing.ts
@@ -3,14 +3,19 @@ import {RouterModule} from '@angular/router';
 
 import {HomeComponent} from '../home/home.component';
 import {ErrorComponent} from '../error/error.component';
+import {BlogDetailComponent} from '../blogDetail/blog-detail.component';
 
 @NgModule({
     imports: [
         RouterModule.forRoot([
+            {path: 'post/:id', component: BlogDetailComponent},
             {path: '', component: HomeComponent},
             {path: '**', component: ErrorComponent}
         ])
     ],
-    exports: [RouterModule]
+    exports: [RouterModule],
+    declarations:[
+        BlogDetailComponent
+    ]
 })
 export class AppRoutingModule{}
